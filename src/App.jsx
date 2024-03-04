4ebf7e762eed20e8db0a0be2be173330f8b17897
Abdul Wahab <abdulwahab@gmail.com>
2024-03-04 23:54:33 +0500

    navbar section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import styles from "./App.module.css";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
-  return <div className={styles.App}>Hi Abdul Wahab</div>;
+  return (
+    <div className={styles.App}>
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import styles from "./Navbar.module.css";
+import { getImageUrl } from "../../utils.js";
+
+const Navbar = () => {
+  const [menuOpen, setMenuOPen] = useState(false);
+
+  return (
+    <nav className={styles.navbar}>
+      <a className={styles.title} href="/">
+        Porfolio
+      </a>
+      <div className={styles.menu}>
+        <img
+          className={styles.menuBtn}
+          src={
+            menuOpen
+              ? getImageUrl("nav/closeIcon.png")
+              : getImageUrl("nav/menuIcon.png")
+          }
+          alt="menuBtn"
+          onClick={() => setMenuOPen(!menuOpen)}
+        />
+        <ul
+          className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
+          onClick={() => setMenuOPen(false)}
+        >
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#experience">Experiences</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          <li>
+            <a href="#contact">contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
