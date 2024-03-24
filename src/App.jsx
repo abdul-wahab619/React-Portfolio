16c7b9aff10aeb0019157148995c377ae59988cb
Abdul Wahab <abdulwahab@gmail.com>
2024-03-24 14:49:04 +0500

    About Section done

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import styles from "./App.module.css";
+import About from "./components/About/About";
 import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
 
@@ -7,6 +8,7 @@ function App() {
     <div className={styles.App}>
       <Navbar />
       <Hero />
+      <About />
     </div>
   );
 }
---
+++ src/components/About/About.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import styles from "./About.module.css";
+import { getImageUrl } from "../../utils";
+
+const About = () => {
+  return (
+    <section className={styles.container} id="about">
+      <h2 className={styles.title}>About</h2>
+      <div className={styles.content}>
+        <img
+          src={getImageUrl("about/aboutImage.png")}
+          alt="about image"
+          className={styles.aboutImage}
+        />
+        <ul className={styles.aboutItems}>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/cursorIcon.png")} alt="curson icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Fontend Developer</h3>
+              <p>
+                I'm a frontend developer with experience in building websites UI
+                and optimized sites
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/serverIcon.png")} alt="Server icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Backend Developer</h3>
+              <p>
+                I have experience developing fast and optimised back-end systems
+                and APIs.
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/uiIcon.png")} alt="UI icon" />
+            <div className={styles.aboutItemText}>
+              <h3>UI Designer</h3>
+              <p>
+                I have designed multiple landing pages and have created design
+                systems as well.
+              </p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+};
+
+export default About;
