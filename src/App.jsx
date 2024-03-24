43f87dffe3da1994961ba6f8aa2a7749b4ea9fe0
Abdul Wahab <abdulwahab@gmail.com>
2024-03-24 16:28:24 +0500

    Projects Section done

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import About from "./components/About/About";
 import Experience from "./components/Experience/Experience";
 import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
+import Projects from "./components/Projects/Projects";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Hero />
       <About />
       <Experience />
+      <Projects />
     </div>
   );
 }
---
+++ src/components/Projects/ProjectCard.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { getImageUrl } from "../../utils";
+import styles from "./ProjectCard.module.css";
+
+const ProjectCard = ({
+  project: { title, imageSrc, description, skills, demo, source },
+}) => {
+  return (
+    <div className={styles.container}>
+      <img
+        src={getImageUrl(imageSrc)}
+        alt={`Image of ${title}`}
+        className={styles.image}
+      />
+      <h3 className={styles.title}>{title}</h3>
+      <p className={styles.description}>{description}</p>
+      <ul className={styles.skills}>
+        {skills.map((skill, id) => {
+          return (
+            <li key={id} className={styles.skill}>
+              {skill}
+            </li>
+          );
+        })}
+      </ul>
+      <div className={styles.links}>
+        <a href={demo} className={styles.link}>
+          Demo
+        </a>
+        <a href={source} className={styles.link}>
+          Source
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectCard;
