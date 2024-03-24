aa68fbc1eee66b0f58823e796c7e72c49706c9fa
Abdul Wahab <abdulwahab@gmail.com>
2024-03-24 15:43:35 +0500

    Experience Section done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import styles from "./App.module.css";
 import About from "./components/About/About";
+import Experience from "./components/Experience/Experience";
 import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
 
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Hero />
       <About />
+      <Experience />
     </div>
   );
 }
